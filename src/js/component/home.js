37bcbe901d2b6dfbf6a3513cb6e80d6d5ed0339c
marie721 <mdarvola@gmail.com>
2020-07-13 19:10:27 +0000

    first commit, set up simple counter

--- src/js/component/home.js
+++ src/js/component/home.js
@@ -1,4 +1,6 @@
 import React from "react";
+import {SimpleCounter} from "./index"
+import PropTypes from "prop-types"
 
 //include images into your bundle
 import rigoImage from "../../img/rigo-baby.jpg";
@@ -6,19 +8,6 @@ import rigoImage from "../../img/rigo-baby.jpg";
 //create your first component
 export function Home() {
 	return (
-		<div className="text-center mt-5">
-			<h1>Hello Rigo!</h1>
-			<p>
-				<img src={rigoImage} />
-			</p>
-			<a href="#" className="btn btn-success">
-				If you see this green button... bootstrap is working
-			</a>
-			<p>
-				Made by{" "}
-				<a href="http://www.4geeksacademy.com">4Geeks Academy</a>, with
-				love!
-			</p>
-		</div>
+		<SimpleCounter/>
 	);
 }
